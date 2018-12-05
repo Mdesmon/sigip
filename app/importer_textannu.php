3a94b069fc11ad046b5257563e0ac52911b1dd3c
Louis Moraes <lmoraes@hotmail.fr>
2018-12-05 17:10:45 +0100

    :bento: Ajout d'un modèle de textannu dans importer_textannu.php

--- app/importer_textannu.php
+++ app/importer_textannu.php
@@ -11,39 +11,47 @@
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
 	<?php
 		include '../includes/head.html';
 	?>
 
-    <title>Importer Textannu</title>
+		<title>Importer Textannu</title>
 
-    <link href="../css/textannu.css" rel="stylesheet">
+		<link href="../css/textannu.css" rel="stylesheet">
 
-  </head>
-  <body>
-  	<?php include '../includes/avatar.php'; ?>
+	</head>
+	<body>
+		<?php include '../includes/avatar.php'; ?>
 	
-    <h1>Importer Textannu</h1>
+		<h1>Importer Textannu</h1>
 
-	<div class="space100"></div>
+		<div class="space100"></div>
 
-    <div class="menu glass">
-    	<form id="form" method="post" action="../api/traitement_textannu.php" enctype="multipart/form-data">
+
+		<div class="menu glass">
+			<form id="form" method="post" action="../api/traitement_textannu.php" enctype="multipart/form-data">
 			Fichier : <input name="file" type="file" />
 			<div class="space20"></div>
 			<div class="btnBlanc" id="btnSubmit" onclick="form.submit()">Envoyer</div>
-    	</form>
-    </div>
-	
+			</form>
+		</div>
+
+		
+		<a class="btnOutline" href="../assets/textannu.xlsx">
+			<div class="icon div downloadFile"></div>
+			Télécharger le modèle
+		</a>
+		
+		
 
 
 
 	<script src="../node_modules/atomjs/atom.js"></script>
-  </body>
+	</body>
 </html>
